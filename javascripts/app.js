beb4dee24818d061ebc78d047d8d2b601e700102
choroshin <choroshin@gmail.com>
2014-07-03 00:02:05 +0300

    Update app.js

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,7 +1,12 @@
 angular.module('numeric', ['numeric']);
-function ctrl($scope,$log){
+function ctrl($scope){
+$scope.events=[];
  $scope.numeric_options = {
-		start: function (event, ui) { $log.info('Slider start'); },
-    stop: function (event, ui) { $log.info('Slider stop'); }
+		start: function (event, ui) { 
+			$scope.events.push('Slider start'); 
+			},
+    stop: function (event, ui) { 
+    	$scope.events.push('Slider stop'); }
  }
+ 
 }
