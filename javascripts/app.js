58b6114e839b131bbe6cc175bcf4ca4681f7516d
choroshin <choroshin@gmail.com>
2014-07-03 00:03:03 +0300

    Update app.js

--- javascripts/app.js
+++ javascripts/app.js
@@ -3,10 +3,12 @@ function ctrl($scope){
 $scope.events=[];
  $scope.numeric_options = {
 		start: function (event, ui) { 
-			$scope.events.push('Slider start'); 
+			$scope.events.push['Slider start']; 
 			},
     stop: function (event, ui) { 
-    	$scope.events.push('Slider stop'); }
+    	$scope.events.push['Slider stop']; 
+    	}
  }
+ 
  
 }
