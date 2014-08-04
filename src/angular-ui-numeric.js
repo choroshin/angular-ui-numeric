7505698a3894708dd23983a7535ec1ceaff4744e
Alex Choroshin <choroshin@gmail.com>
2014-08-04 11:11:44 +0300

    Update angular-ui-numeric.js

--- src/angular-ui-numeric.js
+++ src/angular-ui-numeric.js
@@ -52,11 +52,11 @@ angular.module('ui.numeric', []).directive("numeric", function ($timeout,$compil
                     }, 0);
                 });
                // Update model value from spinner
-                scope.$watch("numericModel", function (newVal) {
-                    if (newVal) {
-                        ngModel.$setViewValue(newVal);
-                    }    
-                }, true);
+             //   scope.$watch("numericModel", function (newVal) {
+             //       if (newVal) {
+             //           ngModel.$setViewValue(newVal);
+             //       }    
+              //  }, true);
                 // Find out if decimals are to be used for spinner
                 angular.forEach(properties, function (property) {
                 // watch for updates
